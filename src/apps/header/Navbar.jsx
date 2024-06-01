6cbbcbc79744550f2d3673b2768de6a4dbaa2771
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-06-01 21:02:23 +0530

    chore: Update form layout for UpdateBatch, UpdateCourse, UpdateResult, and UpdateTestimonials components, fix responsiveness

--- src/apps/header/Navbar.jsx
+++ src/apps/header/Navbar.jsx
@@ -1,27 +1,30 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import DrawerDemo  from "../sidebar/Drawer";
 
 const Navbar = () => {
   return (
     <nav className="px-5 h-14 bg-slate-100 flex justify-between items-center">
-      <a
-        href="/"
+      <div
+       
         className="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl"
         aria-label="logo"
       >
+        <DrawerDemo/>
         <svg
           width="95"
           height="94"
           viewBox="0 0 95 94"
-          className="w-6 h-auto text-gray-800"
+          className="w-6 h-auto text-gray-800 hidden sm:block"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M96 0V47L48 94H0V47L48 0H96Z" />
         </svg>
+      
         The Lakshaya
-      </a>
+      </div>
       <div>
-        <Avatar className='cursor-pointer'>
+        <Avatar className="cursor-pointer">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
